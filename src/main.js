77f8b10af5a05c9fa33eb460f74f4edd7cb782e3
Massimiliano Giroldi <max.devjs@gmail.com>
2018-01-03 16:57:13 -0200

    Add src

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const a = 3;
+const b = 3;
+d = 4;
+
+const c =
+  'rewrewrwerewrweeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr';
+
+// console.log(a, b, c, d);
+
+function ops() {
+  return a + b + c + d;
+}
+
+ops();
